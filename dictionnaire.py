24d16d0ab0904490dc22436a25307011ea45fe45
Michael Rabotin <michael.rabotin@inrae.fr>
2025-01-31 14:25:27 +0100

    fixed mispelling in config var name

--- dictionnaire.py
+++ dictionnaire.py
@@ -84,10 +84,10 @@ config_UH_production_var_json="production"
 config_UH_low_slope_var_json="low_slope"
 config_UH_medium_slope_var_json="medium_slope"
 config_UH_high_slope_var_json="high_slope"
-config_UH_drain_0_var_json="0 : sans drainage "
-config_UH_drain_1_var_json="1 : drainage plein "
-config_UH_drain_2_var_json="2 : mouillère drainée "
-config_UH_drain_3_var_json="3 : mouillère active "
+config_UH_drain_0_var_json="0 : sans drainage"
+config_UH_drain_1_var_json="1 : drainage plein"
+config_UH_drain_2_var_json="2 : mouillère drainée"
+config_UH_drain_3_var_json="3 : mouillère active"
 
 
 # Variables data type :
